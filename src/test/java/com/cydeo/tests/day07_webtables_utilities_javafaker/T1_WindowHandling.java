9287ff84a64ab6e0abfd482039edecde40a5429a
Burockjohn <brkcn87@gmail.com>
2023-07-17 12:35:53 +0300

    day07 - task1 updated based on browser utils

--- src/test/java/com/cydeo/tests/day07_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day07_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,9 +1,9 @@
 package com.cydeo.tests.day07_webtables_utilities_javafaker;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -36,16 +36,9 @@ public class T1_WindowHandling {
         ((JavascriptExecutor) driver).executeScript("window.open('https://facebook.com','_blank');");
 
         //4. Create a logic to switch to the tab where Etsy.com is open
-        for (String each : driver.getWindowHandles()) {
-            driver.switchTo().window(each);
-            System.out.println("Current URL: " + driver.getCurrentUrl());
-            if (driver.getCurrentUrl().contains("etsy")) {
-                break;
-            }
-        }
-
         //5. Assert: Title contains “Etsy”
-        Assert.assertTrue(driver.getTitle().contains("Etsy"));
+        BrowserUtils.switchWindowAndVerify(driver,"etsy","Etsy");
+
     }
 
     @AfterMethod
